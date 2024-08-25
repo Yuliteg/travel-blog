b1bd6210b484dfd28870f995757bd300e8f1f612
Yulia Fedchenko <y.ekler1@gmail.com>
2024-08-25 17:50:57 +0200

    Check if pre-commit work correctly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="flex items-center justify-center min-h-screen bg-gray-100">
+    <main className='flex items-center justify-center min-h-screen bg-gray-100'>
       <h1 className="text-5xl font-bold text-blue-300">Hi!</h1>
     </main>
   );
