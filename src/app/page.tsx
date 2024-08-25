1c07079d115ad6db6b7bd2f4c9187da3c68f5bd0
Yulia Fedchenko <y.ekler1@gmail.com>
2024-08-25 20:11:08 +0200

    Separate Prettier from ESLint to avoid formatting conflicts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-5xl font-bold text-blue-300">Hi!</h1>
+      <h1 className="text-5xl font-bold text-blue-30">Hi!</h1>
     </main>
   );
 }
